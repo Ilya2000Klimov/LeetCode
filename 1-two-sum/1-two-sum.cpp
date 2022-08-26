63306fd362b3afa728f8b4e6264e27749be17721
Ilya Klimov <53608243+Ilya2000Klimov@users.noreply.github.com>
2022-08-25 18:10:38 -0700

    Time: 426 ms (34.84%), Space: 10 MB (99.88%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target)
     {
-        for (int i=0;i<nums.size();i++)
+        for (int i=0;i<nums.size();i++) //Double loop solution suboptimal
         {
             int k=target-nums[i];
             for (int j = i+1; j < nums.size(); j++)
