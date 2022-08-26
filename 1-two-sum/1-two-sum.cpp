3f3af1af1f0fdecab69f658dc4411ef1b49cc9ad
Ilya Klimov <53608243+Ilya2000Klimov@users.noreply.github.com>
2022-08-25 18:44:15 -0700

    Time: 19 ms (68.39%), Space: 11 MB (34.29%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target)
     {
-        for (int i=0;i<nums.size();i++) //Double loop solution suboptimal
+        unordered_map<int,int>map; //Using unordered map
+        for(int i=0;i<nums.size();i++)  //O(n)
         {
-            int k=target-nums[i];
-            for (int j = i+1; j < nums.size(); j++)
+            int k=target-nums[i];   //k = the remainder of the number of target number - curent number => increase space complexity but reduses time complexity
+            if(map.count(k)) //If you can find a non 0 number of needed numbers in the past numbers
             {
-                if (k == nums[j])
-                    return {i, j};
+                return {map[k],i};  //Map[k], pulls  out the index of the right number
             }
+            map[nums[i]]=i; //Puts the index of the current number into the number key of the map
         }
-        
-        return {-1, -1};
+        return {-1,-1}; //if nothing is found, return {-1, -1}
     }
 };
 //vanessa is my girlfriend 
