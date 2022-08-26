30aa1de6c2ae26b16975f543ffeb26e94a335e41
Ilya Klimov <53608243+Ilya2000Klimov@users.noreply.github.com>
2022-08-25 18:04:38 -0700

    Time: 320 ms (38.27%), Space: 10.2 MB (89.40%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        for (int i=0;i<nums.size();i++)
+        {
+            int k=target-nums[i];
+            for (int j = i+1; j < nums.size(); j++)
+            {
+                if (k == nums[j])
+                    return {i, j};
+            }
+        }
+        
+        return {-1, -1};
+    }
+};
+//vanessa is my girlfriend 
+//she is so +cute+XXXDDDDDD
+//HELL000 W000R1D
+//COMPUTER READ THIS OUT LOUD NOW
