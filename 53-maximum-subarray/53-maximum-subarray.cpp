17b25e46f7c0f3f6674bfaa0c12b47c4e61baa19
Ilya Klimov <53608243+Ilya2000Klimov@users.noreply.github.com>
2022-09-05 16:20:22 -0700

    Time: 95 ms (99.70%), Space: 67.8 MB (10.92%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        if (nums.size() == 1) return nums[0];
+        
+        int max = nums[0];
+        int sum = INT_MIN;
+        
+        for (auto &n : nums)
+        {
+            if (sum < 0) sum = n;
+            else sum += n;
+            
+            max = std::max(max, sum);
+        }
+        return (max);
+    }
+};
+
+//not quite right
