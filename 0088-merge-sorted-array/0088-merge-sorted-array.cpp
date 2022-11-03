ee143c55ff3fa37643f264e4a31bcada676c1103
Ilya Klimov <53608243+Ilya2000Klimov@users.noreply.github.com>
2022-11-03 14:35:51 -0700

    Time: 3 ms (80.98%), Space: 9.1 MB (30.47%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n)
+    {
+        int nSize = nums1.size();
+        vector <int> N(nSize, 0);
+        
+        if (m == 0)
+        nums1 = nums2;
+        else if (n == 0);
+        else
+        {
+            for(int i = 0, n1 = 0, n2 = 0; i < nSize; i++)
+            {
+                if (n1 >= m)
+                    N[i] = nums2[n2++];
+                else if(n2 >= n)
+                    N[i] = nums1[n1++];
+                else
+                    N[i] = nums1[n1] < nums2[n2] ? nums1[n1++] : nums2[n2++];
+                
+            }
+        
+            nums1 = N;
+        }
+    }
+};
